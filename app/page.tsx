dfaa298cf2ebe19eb8c0b70c0695c1b09b86bbdb
Dice <98948753+Dice1111@users.noreply.github.com>
2024-12-22 19:46:09 +0800

    pricing&faq

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,17 @@
+import Faq from "@/components/sections/Faq";
+import Features from "@/components/sections/Features";
 import Header from "@/components/sections/Header";
 import Hero from "@/components/sections/Hero";
+import Pricing from "@/components/sections/Pricing";
 
 export default function Home() {
   return (
     <main className="overflow-hidden">
       <Header></Header>
       <Hero></Hero>
+      <Features></Features>
+      <Pricing></Pricing>
+      <Faq></Faq>
     </main>
   );
 }
